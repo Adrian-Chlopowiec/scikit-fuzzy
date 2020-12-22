3f3f459c8d169b304780cd00ed90d81d4fef752d
Adrian Chlopowiec <klopowik@op.pl>
2020-12-22 17:36:40 +0100

    Written KM-Algorithm

--- skfuzzy/defuzzifyType2/defuzz.py
+++ skfuzzy/defuzzifyType2/defuzz.py
@@ -7,6 +7,8 @@ import numpy as np
 from .exceptions import EmptyMembershipError, InconsistentMFDataError
 from ..image.arraypad import pad
 
+EPSILON = 1e-6
+
 
 def defuzz(x, mfx):
     """
@@ -31,10 +33,51 @@ def defuzz(x, mfx):
     - InconsistentMFDataError : When the length of the 'x' and the fuzzy
         membership function arrays are not equal.
     """
+
     teta = np.mean(mfx[0], mfx[1])
-    tetaL = teta
-    tetaR = teta
+    yL, L = __find_y(x, teta.copy(), mfx[1], mfx[0])
+    yR, R = __find_y(x, teta.copy(), mfx[0], mfx[1])
 
-    cprim = np.average(tetaL, weights=x)
+    yL = __calc_y_val(x, L, mfx[1], mfx[0])
+    yR = __calc_y_val(x, R, mfx[0], mfx[1])
+    return (yL + yR) / 2
 
 
+def __find_y(universe, teta, first_mf, second_mf):
+    def calc_c_second(new_teta, c):
+        k = 0
+        for i in universe:
+            if i >= c:
+                k -= 1
+                break
+            k += 1
+
+        for i, l_val, r_val in enumerate(first_mf, second_mf):
+            if i <= k:
+                new_teta[i] = l_val
+            else:
+                new_teta[i] = r_val
+
+        return np.average(universe, weights=new_teta), new_teta, k
+
+    cprim = np.average(universe, weights=teta)
+    csecond, teta, k = calc_c_second(teta, cprim)
+
+    while csecond - cprim <= EPSILON:
+        cprim = csecond
+        csecond, teta, k = calc_c_second(teta, cprim)
+
+    return csecond, k
+
+
+def __calc_y_val(universe, breakpoint, first_mf, second_mf):
+    def sum_of_series(array, mf, start, end):
+        acc = 0
+        for i in range(start, end):
+            acc += array[i] * mf[i]
+        return acc
+
+    return ((sum_of_series(universe, first_mf, 0, breakpoint + 1)
+             + sum_of_series(universe, second_mf, breakpoint + 1, len(universe)))
+            / (sum_of_series(np.ones_like(universe), first_mf, 0, breakpoint + 1)
+               + sum_of_series(np.ones_like(universe), second_mf, breakpoint + 1, len(universe))))
